afd179facdabc9ee979cbce6780d72320e6b8edd
PhantomMG <matiasgonzalezsn@gmail.com>
2020-09-06 16:37:55 -0300

    First Commit

---
+++ src/build/precache-manifest.b7f064318f8adb5bcce428d3cd5539d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf407775168283f18aabc7d4e60223e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f29bd7ec41a845b6e3",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f29bd7ec41a845b6e3",
+    "url": "/static/js/main.da284d9d.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
